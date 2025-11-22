8a0069a2243d7c5b10203e7bd04822d29b12d206
myanikaislam1920@gmail.com <myanikaislam1920@gmail.com>
2025-11-22 08:32:23 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
+import ownerRouter from "./routes/ownerRoutes.js";
+
+const PORT = process.env.PORT || 3000;
+
+//Initialize express app
+const app = express();
+
+//connect database
+await connectDB()
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send("server is running"))
+app.use('/api/user', userRouter)
+app.use('/api/owner', ownerRouter)
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
+

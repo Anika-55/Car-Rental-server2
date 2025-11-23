955fa825148c303f5450ea7d391ddf7692ec441e
myanikaislam1920@gmail.com <myanikaislam1920@gmail.com>
2025-11-23 13:18:53 +0600

    added imagekit

--- controllers/ownerController.js
+++ controllers/ownerController.js
@@ -1,6 +1,9 @@
+import imagekit from "../configs/imageKit.js";
 import User from "../models/User.js";
+import Car from '../models/Car.js';
+import fs from "fs";
 
-
+//api to change the role of user
 export const changeRoleToOwner = async (req, res) => {
     try {
         const { _id } = req.user;
@@ -9,5 +12,76 @@ export const changeRoleToOwner = async (req, res) => {
     } catch (error) {
         console.log(error.message);
         res.json({success:false, message: "Now you can list cars"})
+    }
+}
+
+//API to list car
+
+export const addCar = async (req, res) => {
+    try {
+        const { _id } = req.body;
+        let car = JSON.parse(req.body.carData);
+        const imageFile = req.file;
+        
+        //Upload image to imagekit
+        const fileBuffer = fs.readFileSync(imageFile.path)
+      const response =  await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder:'/cars'
+      })
+        
+        // optimization through imagekit URL transformation
+        var optimizedImageUrl = imagekit.url({
+            path: response.filePath,
+            transformation: [
+                { width: '1280' }, //width resizing
+                { quality: 'auto' }, // Auto compression
+                {format: 'webp'} // convert to modern format
+            ]
+        })
+        
+        const image = optimizedImageUrl;
+        await Car.create({ ...car, owner: _id, image })
+        
+        res.json({ success: true, message: "Car Added" })
+        
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message: error.message})
+    }
+}
+
+//API to List Owner Cars
+
+export const getOwnerCars = async (req, res) => {
+    try {
+        const { _id } = req.user;
+        const cars = await Car.find({ owner: _id })
+        res.json({success:true, cars})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message: error.message})
+    }
+}
+
+// API to Toggle Car Availability
+export const toggleCarAvailability = async (req, res) => {
+    try {
+        const { _id } = req.user;
+        const { carId } = req.body;
+        const car = await Car.findById(carId)
+
+        //checking is car belongs to the user
+        if (car.owner.toString() !== _id.toString()) {
+            return res.json({success:false, message: "Unauthorized"})
+        }
+
+        car.isAvaliable = !car.isAvaliable;
+        await car.save()
+        res.json({success:true, message: "Availability Toggled"})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message: error.message})
     }
 }
